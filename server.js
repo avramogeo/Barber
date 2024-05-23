251d455d2d872ea7b73567172b04ef5b12270972
Georgios Avramopoulos <avramogeo@gmail.com>
2024-05-23 22:09:50 +0300

    new

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const { checkUser, addUser} = require('./database'); // Import connection object
-const connection = require('./database');
+const connection = require('./database'); // Import connection object
 
 // Create an Express app
 const app = express();
@@ -12,6 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware setup
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -22,7 +22,10 @@ app.get('/', (req, res) => {
 
 app.post('/login', (req, res) => {
     const { username } = req.body;
-    
+
+    // Log the received username to verify it's correctly parsed
+    console.log('Received username:', username);
+
     // Query the database to check if the username exists
     const query = 'SELECT * FROM users WHERE username = ?';
     connection.query(query, [username], (err, results) => {
@@ -32,17 +35,29 @@ app.post('/login', (req, res) => {
             return;
         }
 
+        // Log the results of the query
+        console.log('Query results:', results);
+
         // Check if results are returned
         if (results.length > 0) {
             // User found
+            console.log('User found:', results[0]);
             res.json({ success: true });
         } else {
             // No results, user does not exist
-            res.status(401).json({ success: false, message: "User does not exist" });
+            console.log('User does not exist');
+            res.json({ success: false });
         }
     });
 });
 
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Home.html'));
+});
+
+app.get('/store/:name', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'store.html'));
+});
 
 // Start the server
 app.listen(PORT, () => {
